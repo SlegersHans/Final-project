e1b1ada3528e79157e23786f832cf497b83827ef
Hans Slegers <hans.slegers@student.kuleuven.be>
2023-05-29 02:45:24 +0200

    POI okay

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,59 +1,13 @@
-<script>
-	import Counter from './Counter.svelte';
-	import welcome from '$lib/images/svelte-welcome.webp';
-	import welcome_fallback from '$lib/images/svelte-welcome.png';
-</script>
+<h1>
+	Data Visualisation in Data Science: final project
+</h1>
+<p> This is the webpage for the final project of the course Data Visualisation in Data Science, given by Prof Jan Aerts, Jelmer Bot.</p>
+<ul> Student information:
+	<li> Name: Hans Slegers</li>
+	<li> University: KU Leuven</li>
+	<li> Study: Mathematical Engineering</li>
+	<li> Student number: r0710477</li>
 
-<svelte:head>
-	<title>Home</title>
-	<meta name="description" content="Svelte demo app" />
-</svelte:head>
-
-<section>
-	<h1>
-		<span class="welcome">
-			<picture>
-				<source srcset={welcome} type="image/webp" />
-				<img src={welcome_fallback} alt="Welcome" />
-			</picture>
-		</span>
-
-		to your new<br />SvelteKit app
-	</h1>
-
-	<h2>
-		try editing <strong>src/routes/+page.svelte</strong>
-	</h2>
-
-	<Counter />
-</section>
-
-<style>
-	section {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		flex: 0.6;
-	}
-
-	h1 {
-		width: 100%;
-	}
-
-	.welcome {
-		display: block;
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
-
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
-	}
-</style>
+</ul>
+<p>Link to the overview page: <a style="font-weight: bold;" href="./overview">here</a>.</p>
+<p>Link to the overview test page: <a style="font-weight: bold;" href="./overview_test">here</a>.</p>
---
+++ src/routes/overview/+page.svelte
@@ -0,0 +1,46 @@
+<script>
+    import '../styles.css'
+
+    let datapoints = [{x: 100, y: 100},
+                        {x: 150, y: 275},
+                        {x: 10, y: 101},
+                        {x: 80, y: 183},
+                        {x: 350, y: 45},
+                        {x: 201, y: 285},
+                        {x: 150, y: 306},
+                        {x: 90, y: 102},
+                        {x: 73, y: 39},
+                        {x: 332, y: 269}]
+
+    export let data;
+
+    const rescale = function(x, domain_min, domain_max, range_min, range_max) {
+      return ((range_max - range_min)*(x-domain_min))/(domain_max-domain_min) + range_min
+  }
+</script>
+
+<style>
+    svg {
+        border: 1px;
+        border-style: solid;
+    }
+</style>
+
+<h1> Hans Slegers - KU Leuven - r0710477</h1>
+<svg width=600 height=600>
+    <!-- {#each datapoints as dp}
+    <circle cx={dp.x} cy={dp.y} r=10 />
+    {/each} -->
+    {#each data.PoI as poi}
+    <circle cx={rescale(poi.long, 24.82, 24.92, 0, 600)} 
+            cy={rescale(poi.lat, 36.045,36.0915, 600, 0)} 
+            r=3 
+            class:cat={poi.type === "catering" }
+            class:dom={poi.type === "domestic" }
+            class:house={poi.type === "housing" }
+            class:prof={new String(poi.type).valueOf() === new String("professional").valueOf() }
+            class:other={poi.type === "other" }
+            />
+    {/each}
+
+</svg>
